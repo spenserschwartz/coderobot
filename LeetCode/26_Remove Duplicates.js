f9b9369f70c28d9f905f086d7dd027155b364009
Spenser Schwartz <spensernet@gmail.com>
2020-10-20 20:01:34 -0700

    LeetCode singleNumber

--- LeetCode/26_Remove Duplicates.js
+++ LeetCode/26_Remove Duplicates.js
@@ -3,14 +3,26 @@ Given a sorted array nums, remove the duplicates in-place such that each element
 */
 
 
+// const removeDuplicates = (nums) => {
+//   let len = 1;
+//   for (let j = 1; j < nums.length; j += 1) {
+//     if (nums[j] !== nums[j - 1]) nums[len++] = nums[j];
+//   }
+//   return len;
+// }
+
+
+
 const removeDuplicates = (nums) => {
-  let len = 1;
-  for (let j = 1; j < nums.length; j += 1) {
-    if (nums[j] !== nums[j - 1]) nums[len++] = nums[j];
+  let length = 1; 
+
+  for(let i = 1; i < nums.length; i += 1) {
+    if (nums[i] !== nums[i - 1]) length += 1;
   }
-  return len;
+  return length;
 }
 
 
 let nums = [1,1,2,2,3,3];
 console.log(removeDuplicates(nums));
+
