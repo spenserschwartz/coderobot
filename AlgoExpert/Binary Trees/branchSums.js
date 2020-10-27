58130d6765b2ee11bd9f57006afdd19e7cd8c771
Spenser Schwartz <spensernet@gmail.com>
2020-10-27 12:48:40 -0700

    AlgoExpert branchSums

--- AlgoExpert/Binary Trees/branchSums.js
+++ AlgoExpert/Binary Trees/branchSums.js
@@ -8,10 +8,21 @@ class BinaryTree {
   }
 }
 
-function branchSums(root) {
-  let arr = [];
-  let sum = root.value;
-  if (root.left)
+function branchSums(root, sum = 0, arr = []) {
+
+  if (!root) return;
+
+  sum += root.value;
+  if (!root.left && !root.right) {
+    arr.push(sum);
+    if (arr.length === 1) return arr;
+    return;
+  }
+
+  branchSums(root.left, sum, arr);
+  branchSums(root.right, sum, arr);
+
+  return arr;
 }
 
 // Do not edit the lines below.
--- LeetCode/38_Count and Say.js
+++ LeetCode/38_Count and Say.js
@@ -4,17 +4,18 @@ const countAndSay = n => {
   let memo = {};
   let arr = [];
 
-  for (let digit of n) {
-    if (memo[digit]) memo[digit] += 1;
-    else if (Object.keys(memo).length > 0) {
-      arr.push([Number(Object.keys(memo)[0]), Object.values(memo)[0]]);
-      memo = {};
-      memo[digit] = 1;
-    } 
+  for (let i = 0; i < n.length; i += 1) {
+    if (memo[n[i]]) memo[n[i]] += 1;
+    else if (Object.keys(memo).length === 1 && n[i] !== n[i - 1]) {
+      arr.push(memo);
+      
+    } else {
+      memo[n[i]] = 1;
+    }
   }
-
-  return memo;
+  console.log(memo);
+  return arr;
 }
 
-let n = '3322251';
+let n = '3344451';
 console.log(countAndSay(n));
