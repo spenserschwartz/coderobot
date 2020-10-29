bc622825014f246170996f118acf26c404f001d5
Spenser Schwartz <spensernet@gmail.com>
2020-10-28 19:56:32 -0700

    Finish LeetCode isSubtree

--- LeetCode/572_Subtree of Another Tree.js
+++ LeetCode/572_Subtree of Another Tree.js
@@ -1,5 +1,17 @@
 // https://leetcode.com/problems/subtree-of-another-tree/
 
+// const isSubtree = (s, t) => {
+//   if (!s) return !t;
+//   return isEqual(s, t) || isSubtree(s.left, t) || isSubtree(s.right, t);
+// }
+
+// const isEqual = (root1, root2) => {
+//   if (!root1 || !root2) return !root1 && !root2;
+//   if (root1.val !== root2.val) return false;
+
+//   return isEqual(root1.left, root2.left) && isEqual(root1.right, root2.right);
+// }
+
 const isSubtree = (s, t) => {
   if (!s) return !t;
   return isEqual(s, t) || isSubtree(s.left, t) || isSubtree(s.right, t);
@@ -9,5 +21,5 @@ const isEqual = (root1, root2) => {
   if (!root1 || !root2) return !root1 && !root2;
   if (root1.val !== root2.val) return false;
 
-  return isEqual(root1.left, root2.left) && isEqual(root1.right, root2.right);
+  return isEqual(root1.left, root2.left) && isEqual(root2.left, root2.right)
 }
