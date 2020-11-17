521c2ef63b5721630bcf5ab7e63dea664cec5653
Spenser Schwartz <spensernet@gmail.com>
2020-11-16 19:47:59 -0800

    LeetCode Permutation in a String

--- LeetCode/567_Permutation in String.js
+++ LeetCode/567_Permutation in String.js
@@ -6,31 +6,20 @@ Explanation: s2 contains one permutation of s1 ("ba").
 */
 
 // ! Using sort
-const checkInclusion = (s1, s2) => {
-  const s1sort = s1.split('').sort().join(''); 
-
-  for (let i = 0; i < s2.length - s1.length + 1; i++) {
-    if (s1.includes(s2[i])) {
-      const s2sub = s2.substring(i, i + s1.length).split('').sort().join('');
-      if (s2sub === s1sort) return true;
-    }
-  }
-  return false;
-}
-
-
 // const checkInclusion = (s1, s2) => {
-//   const subLength = s1.length;
-  
-//   for (let i = 0; i < s2.length - subLength + 1; i++) {
+//   const s1sort = s1.split('').sort().join(''); 
+
+//   for (let i = 0; i < s2.length - s1.length + 1; i++) {
 //     if (s1.includes(s2[i])) {
-//       const s2Check = s2.substring(i, i + subLength);
-//       console.log(s2Check);
+//       const s2sub = s2.substring(i, i + s1.length).split('').sort().join('');
+//       if (s2sub === s1sort) return true;
 //     }
 //   }
+//   return false;
 // }
+
 
 let s1 = "ab", s2 = "eidbaooo";
 let s3 = 'ab', s4 = 'eidboaoo';
-console.log(checkInclusion(s1, s2));
-console.log(checkInclusion(s3, s4));
+// console.log(checkInclusion(s1, s2));
+// console.log(checkInclusion(s3, s4));
