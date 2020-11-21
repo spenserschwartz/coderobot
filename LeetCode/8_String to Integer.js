dba7a15992cfb2353be92295a2aa024c33675396
Spenser Schwartz <spensernet@gmail.com>
2020-11-21 01:13:12 -0800

    LeetCode 8, edge cases still need work

--- LeetCode/8_String to Integer.js
+++ LeetCode/8_String to Integer.js
@@ -1,5 +1,7 @@
 // https://leetcode.com/problems/string-to-integer-atoi/
 
+
+// ! Logic is fine, work out edge cases
 const myAtoi = s => {
   while (s[0] === ' ') s = s.substring(1);
   let first = s[0]
@@ -14,7 +16,7 @@ const myAtoi = s => {
   let num = 0;
   while (!isNaN(s[0])) {
     if (s[0] === ' ') break;
-    num *= 10;
+    num *= 10;s
     num += Number(s[0])
     s = s.substring(1);
   }
