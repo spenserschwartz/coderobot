eaf67e9df94d0e09460e8117bb9bb9298ec709ce
Spenser Schwartz <spensernet@gmail.com>
2020-11-21 01:12:46 -0800

    LeetCode 8, edge cases still need work

---
+++ LeetCode/8_String to Integer.js
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/string-to-integer-atoi/
+
+const myAtoi = s => {
+  while (s[0] === ' ') s = s.substring(1);
+  let first = s[0]
+  
+  let isPos = true;
+  if (s[0] === '+') s = s.substring(1);
+  else if (s[0] === '-') {
+    isPos = false;
+    s = s.substring(1);
+  }
+
+  let num = 0;
+  while (!isNaN(s[0])) {
+    if (s[0] === ' ') break;
+    num *= 10;
+    num += Number(s[0])
+    s = s.substring(1);
+  }
+
+  
+  num = isPos ? num : num * -1;
+  if (num > Math.pow(2,31)) return Math.pow(2, 31) - 1;
+  if (num < Math.pow(-2, 31)) return Math.pow(-2, 31);
+
+  return num;
+}
+
+let str = "-4193 with words"
+console.log(myAtoi(str));
