05867f1f71b4ed6535264c2a975100611d760853
Spenser Schwartz <spensernet@gmail.com>
2020-11-24 12:34:21 -0800

    LeetCode Count Primes

--- LeetCode/204_Count Primes.js
+++ LeetCode/204_Count Primes.js
@@ -1,15 +1,17 @@
 // https://leetcode.com/problems/count-primes/
 
+// ! Number of prime numbers LESS THAN n
+
 const countPrimes = n => {
-  if (n < 2) return 0;
+  if (n < 3) return 0;
   let count = 1;
 
-  for (let i = 3; i <= n; i += 2) {
+  for (let i = 3; i < n; i+=2) {
     let isPrime = true;
     for (let j = 3; j <= Math.sqrt(i); j += 2) {
       if (i % j === 0) isPrime = false;
     }
-    if (isPrime === true) count += 1;
+    if (isPrime === true) count++;
   }
 
   return count;
