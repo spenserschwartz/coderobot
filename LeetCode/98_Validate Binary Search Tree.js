ea4352755cf55816ffbff06e784a627e92951941
Spenser Schwartz <spensernet@gmail.com>
2020-11-23 19:12:39 -0800

    LeetCode Validate Binary Search Tree with explanation

--- LeetCode/98_Validate Binary Search Tree.js
+++ LeetCode/98_Validate Binary Search Tree.js
@@ -1,5 +1,12 @@
 // https://leetcode.com/problems/validate-binary-search-tree/
 
+/*
+Explanation: If there's no root, return true (valid tree);
+             If curr val is >= set max or <= set min, not a valid tree
+             If nothing has returned yet, check the valid trees down the left & right side
+             The max value is set when going left (root.val will now be the max)
+             The min value is set when going right (root.val will not be the min)
+*/
 
 const isValidBST = (root, min = -Infinity, max = Infinity) => {
   if (!root) return true;
