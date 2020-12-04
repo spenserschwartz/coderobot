24a29f56330c209f19ccd09bbcf07d0c7f11b319
Spenser Schwartz <spensernet@gmail.com>
2020-12-03 17:40:25 -0800

    165 update

--- LeetCode/165_Compare Version Numbers.js
+++ LeetCode/165_Compare Version Numbers.js
@@ -7,15 +7,16 @@ const compareVersion = (v1, v2) => {
 
   while (v1.length < maxLength) v1.push('0');
   while (v2.length < maxLength) v2.push('0');
-  
-  v1.forEach((el, i) => {
-    console.log(el);
-    console.log(v2[i])
-    if (el === '1') return 3;
-  })
+
+  for (let i = 0; i < v1.length; i++) {
+    const num1 = parseInt(v1[i]), num2 = parseInt(v2[i]);
+    if (num1 !== num2) return num1 > num2 ? 1 : -1
+  }
+
+  return 0;
 }
 
-const version1 = "1.02", version2 = "1.001"
+const version1 = "1.01", version2 = "1.001"
 console.log(compareVersion(version1, version2))
 
 // const version3 = "1.0", version4 = "1.0.0"
