7c3fe11b85914ed8fb6dfe10ca4c8621c16c8585
Spenser Schwartz <spensernet@gmail.com>
2020-12-20 17:09:56 -0800

    LeetCode 235 Lowest Common Ancestor BST

---
+++ LeetCode/235_Lowest Common Ancestor of a BST.js
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+
+const lowestCommonAncestor = (root, p, q) => {
+  if (!root || root === p || root === q) return root;
+
+  const left = lowestCommonAncestor(root.left, p, q);
+  const right = lowestCommonAncestor(root.right, p, q);
+
+  if (left && right) return root;
+  return left || right;
+}
