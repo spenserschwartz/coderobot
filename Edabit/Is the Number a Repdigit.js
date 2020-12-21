436592637f1f141778a956dcaa601af0ae7ef37a
Spenser Schwartz <spensernet@gmail.com>
2020-12-21 11:03:55 -0800

    Is the Number a RepDigit

--- Edabit/Is the Number a Repdigit.js
+++ Edabit/Is the Number a Repdigit.js
@@ -1,9 +1,8 @@
 // https://edabit.com/challenge/8vSpPNjNx6JBcvxdc
 
 const isRepdigit = num => {
-  if (num === 0) return true;
-  
   let string = num.toString();
+  if (string.length === 1) return true;
   return string[0] === string[1];
 }
 
