6ce5062e9b0f2728afa6ebff40295f14dbd3bf13
Spenser Schwartz <spensernet@gmail.com>
2021-01-05 12:10:21 -0800

    Edabit Exponents of Prime Factors

---
+++ Edabit/Exponents of Prime Factors.js
@@ -0,0 +1,14 @@
+// https://edabit.com/challenge/6k2QepzApP5zHejWE
+
+const productEqualTarget = (arr, target) => {
+  return arr.map(el => {
+    let count = 0;
+    while (target % el === 0) {
+      target /= el;
+      count++;
+    }
+    return count;
+  })
+}
+
+console.log(productEqualTarget([2, 3, 5], 600))
