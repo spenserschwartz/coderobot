600b82ed067cb983c7aa80b9d78bb5909cb4725f
Spenser Schwartz <spensernet@gmail.com>
2021-01-06 09:37:55 -0800

    Edabit Find Value in Binary Tree

---
+++ Edabit/Find Value in Binary Tree.js
@@ -0,0 +1,17 @@
+// https://edabit.com/challenge/GGDMJ33wg8y3EHQfP
+
+// ! One line, using flat method
+// const valueInTree = (tree, val) => {
+//   return tree.flat(Infinity).includes(val);
+// }
+
+const valueInTree = (tree, val) => {
+  for (let el of tree) {
+    if (Array.isArray(el) && valueInTree(el, val)) return true;
+    if (el === val) return true; 
+  }
+  return false;
+}
+
+const arr = [3, [ 8, [ 5, null, null], null], [ 7, null, null]];
+console.log(valueInTree(arr, 5))
