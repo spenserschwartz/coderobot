8344379e6ca40ac8d590e12985692c35113cbefc
Spenser Schwartz <spensernet@gmail.com>
2021-01-11 10:04:00 -0800

    Edabit Persistence

--- Edabit/Persistence.js
+++ Edabit/Persistence.js
@@ -1,6 +1,7 @@
 // https://edabit.com/challenge/vHvu4Wis8RhmQbwXm
 
-const additivePersistence = (n, count = 1) => {
+const additivePersistence = (n, count = 0) => {
+  if (n < 10) return count;
   let sum = 0;
 
   while (n > 0) {
@@ -9,14 +10,16 @@ const additivePersistence = (n, count = 1) => {
     n = Math.floor(n / 10)
   }
 
-  return sum < 10 ? count : additivePersistence(sum, count + 1)
+  return sum < 10 ? count + 1 : additivePersistence(sum, count + 1)
 }
 
 console.log(additivePersistence(1679583));
 
 
 
-const multiplicativePersistence = (n, count = 1) => {
+const multiplicativePersistence = (n, count = 0) => {
+  if (n < 10) return count;
+
   let product = 1;
   while (n > 0) {
     let int = n % 10;
@@ -24,7 +27,7 @@ const multiplicativePersistence = (n, count = 1) => {
     n = Math.floor(n / 10);
   }
 
-  return product < 10 ? count : multiplicativePersistence(product, count + 1);
+  return product < 10 ? count + 1 : multiplicativePersistence(product, count + 1);
 }
 
 console.log(multiplicativePersistence(123456));
