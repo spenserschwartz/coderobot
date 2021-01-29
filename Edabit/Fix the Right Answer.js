4e737a1f220ae24407138238b648d54a1e1a9a85
Spenser Schwartz <spensernet@gmail.com>
2021-01-29 10:21:45 -0800

    Edabit Fix the Right Answer

--- Edabit/Fix the Right Answer.js
+++ Edabit/Fix the Right Answer.js
@@ -2,6 +2,30 @@
 
 const postfix = expr => {
   const arr = expr.split(' ');
+  const nums = [];
+  const ops = [];
+
+  arr.forEach(el => isNaN(el) ? ops.push(el) : nums.push(Number(el)))
+
+  let result = nums[0];
+
+  for (let i = 0; i < ops.length; i++) {
+    switch(ops[i]) {
+      case '+': 
+        result += nums[i + 1]
+        break;
+      case '-':
+        result -= nums[i + 1]
+        break;
+      case '*':
+        result *= nums[i + 1]
+        break;
+      case '/':
+        result /= nums[i + 1]
+        break;
+    }
+  }
+  return result;
 }
 
 const expr = "8 4 / 9 * 3 1 * /";
