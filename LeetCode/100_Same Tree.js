3267b4d695e6fd238136de43d51b83e319dfecde
Spenser Schwartz <spensernet@gmail.com>
2021-02-02 18:11:45 -0800

    LeetCode 100 Same Tree

---
+++ LeetCode/100_Same Tree.js
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/same-tree/
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+
+const isSameTree = (p, q) => {
+  if (!p && !q) return true;
+  if (!p || !q || p.val !== q.val) return false;
+
+  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+}
